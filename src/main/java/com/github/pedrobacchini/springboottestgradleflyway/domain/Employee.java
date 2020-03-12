e268dd6f3de37456bebf277db90552395416e458
Pedro Bacchini <pedrobacchini@outlook.com>
2020-03-12 17:15:05 -0300

    Create Employee

---
+++ src/main/java/com/github/pedrobacchini/springboottestgradleflyway/domain/Employee.java
@@ -0,0 +1,26 @@
+package com.github.pedrobacchini.springboottestgradleflyway.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Table(name = "person")
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Size(min = 3, max = 20)
+    private String name;
+
+    public Employee(@Size(min = 3, max = 20) String name) {
+        this.name = name;
+    }
+}
