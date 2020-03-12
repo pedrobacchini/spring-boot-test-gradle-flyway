5f1aa545c3963dd9f403c5aa61c07e89d191c7e3
Pedro Bacchini <pedrobacchini@outlook.com>
2020-03-12 16:58:12 -0300

    Create Project

---
+++ src/main/java/com/github/pedrobacchini/springboottestgradleflyway/SpringBootTestGradleFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.springboottestgradleflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestGradleFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestGradleFlywayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/pedrobacchini/springboottestgradleflyway/SpringBootTestGradleFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.springboottestgradleflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestGradleFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
